e40f3032c059a7d0d7728e751f12bd5b99a40e27
Fortune Ekeruo <legalmody@gmail.com>
2017-09-19 18:20:55 +0100

    Root component index.js file

---
+++ app/components/index.js
@@ -0,0 +1,8 @@
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+
+export default angular.module('App.components', [uiRouter])
+    .config()
+    .component()
+    .service()
+    .name
