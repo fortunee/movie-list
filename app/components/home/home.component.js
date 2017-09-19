87781b2fcc0799d0871797762dda52fea247dc55
Fortune Ekeruo <legalmody@gmail.com>
2017-09-19 21:02:08 +0100

    Setup home component

---
+++ app/components/home/home.component.js
@@ -0,0 +1,11 @@
+const homeCtrl = () => {
+
+}
+
+export const homeComponent = {
+    templateUrl: './app/components/home/home.template.html',
+    controller: homeCtrl,
+    bindings: {
+        home: '='
+    }
+}
