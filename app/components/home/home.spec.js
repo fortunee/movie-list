291c497b0186bebae48939bffa98bf6cd629f6fb
Fortune Ekeruo <legalmody@gmail.com>
2017-09-22 11:21:07 +0100

    Add a home component test with really easy specs for testing purposes

---
+++ app/components/home/home.spec.js
@@ -0,0 +1,9 @@
+describe('Home Component', () => {
+    it('Should run test', () => {
+        expect(2 * 2).toEqual(4);
+    })
+
+    it('Should run another test', () => {
+        expect(2 + 3).toEqual(5);
+    })
+})
--- spec/specs.webpack.js
+++ spec/specs.webpack.js
@@ -2,7 +2,7 @@ require('core-js');
 require('core-js/es5');
 require('babel-polyfill');
 
-require('app.module');
+require('./../app/app.module');
 require('angular-mocks');
 
 const context = require.context('./../app', true, /.spec\.js$/);
