d0f3b2d040e9dfeb0fdbeb43ab8ac47de10ab8fb
Fortune Ekeruo <legalmody@gmail.com>
2017-09-28 08:29:41 +0100

    Add a two way data binding and a button that calls a changeMessage method which changes the message

--- app/components/home/home.component.js
+++ app/components/home/home.component.js
@@ -4,11 +4,15 @@ class homeCtrl {
     constructor() {
         this.message = 'home controller as VM';
     }
+
+    changeMessage() {
+        this.message = 'Another message';
+    }
 }
 
 export const homeComponent = {
     templateUrl: './app/components/home/home.template.html',
-    controllerAs: "vm",
+    controllerAs: "model",
     controller: homeCtrl,
     bindings: {
         home: '='
