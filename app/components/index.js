8035829d6e4bd43bac96f301ff52d922df86e6aa
Fortune Ekeruo <legalmody@gmail.com>
2017-09-29 14:05:40 +0100

    Register the about component page in the index module

--- app/components/index.js
+++ app/components/index.js
@@ -4,6 +4,8 @@ import uiRouter from 'angular-ui-router';
 import routes from './index.routes';
 
 import { moviesComponent } from './movies/movies.component';
+import { aboutComponent } from './about/about.component';
+
 import { movieRatingComponent } from './shared/move-rating.component';
 import { navBarComponent } from './shared/navbar.component';
 import { footerComponent } from './shared/footer.component';
@@ -13,6 +15,7 @@ import './index.style.scss';
 export default angular.module('App.components', [uiRouter])
     .config(routes)
     .component('moviesComponent', moviesComponent)
+    .component('aboutComponent', aboutComponent)
     .component('movieRatingComponent', movieRatingComponent)
     .component('navBarComponent', navBarComponent)
     .component('footerComponent', footerComponent)
