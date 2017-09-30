e800e7b188503ee24f93cd740e8b8c1e58938d12
Fortune Ekeruo <legalmody@gmail.com>
2017-09-30 15:02:52 +0100

    fetchMovies method implemented

--- app/services/movie.service.js
+++ app/services/movie.service.js
@@ -2,4 +2,9 @@ export default class MovieService {
     constructor($http) {
         this.$http = $http;
     }
+
+    fetchMovies() {
+        const endPoint = 'api/movies.json';
+        return this.$http.get(endPoint);
+    }
 }
